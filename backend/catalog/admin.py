2039165ba4ea3c1703f4445a50fb37f569407158
Alejandro Jacome <jacomedelgadoa@ciencias.unam.mx>
2025-11-26 10:44:53 -0600

    agregados sistema de admin

--- backend/catalog/admin.py
+++ backend/catalog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Pelicula
 
-# Register your models here.
+
+# Opción Básica:
+# admin.site.register(Pelicula)
+
+# Opción "Pro" (Para ver columnas y filtros):
+@admin.register(Pelicula)
+class PeliculaAdmin(admin.ModelAdmin):
+    # Qué columnas ver en la lista
+    list_display = ('id', 'nombre', 'productora', 'año', 'director', 'calificacion')
+
+    # Barra de búsqueda
+    search_fields = ('idpelicula', 'nombre', 'director')
+
+    # Filtros laterales
+    list_filter = ('año', 'productora')
+
+    # Orden por defecto
+    ordering = ('-id',)
